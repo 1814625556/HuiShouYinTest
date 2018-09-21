47dc0d358fd358987a4b87cbbaf14286897fdd88
chenchang <chenchang@kezaihui.com>
2018-09-21 19:05:58 +0800

    导航 自定义控件引用测试

--- WpfTest2/MainWindow.xaml.cs
+++ WpfTest2/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +21,34 @@ namespace WpfTest2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly NavigationService _ns;      //获取导航控件
         public MainWindow()
         {
             InitializeComponent();
+            _ns = Application.Current.Windows.OfType<MainWindow>().Single().Frame.NavigationService;
+            //Load();
         }
+
+        private void BtnDaoHangClick(object sender,RoutedEventArgs e)
+        {
+            // 导航
+            //var navigationService = Application.Current.Windows.OfType<MainWindow>().Single().Frame.NavigationService;      //获取导航控件
+            //navigationService.Navigate(new Uri("pack://application:,,,/MainPages/Navigation1.xaml"));
+            //--------------------------------------------------------------------------------------------------------
+
+            // 用户自定义控件
+            this.PopUp.Content = new UserControls.UserControl1();
+        }
+
+        public void Load()
+        {
+            _ns.Navigate(new Uri("pack://application:,,,/MainPages/Navigation1.xaml"));
+        }
+
+        public void RedirectToPage(object sender, NavigationEventArgs e)
+        {
+            
+        }
+
     }
 }
