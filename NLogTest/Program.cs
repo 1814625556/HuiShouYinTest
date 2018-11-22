3cb8638c20716ed8a0f9ca3091c853228a9448f0
chenchang <chenchang@kezaihui.com>
2018-11-22 19:15:23 +0800

    Nlog 测试

---
+++ NLogTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace NLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var config = new NLog.Config.LoggingConfiguration();
+
+            //var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "ccfile.txt" };
+            //var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            //config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            //config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            //NLog.LogManager.Configuration = config;
+
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            //logger.Info("Hello {0}", "Earth");
+            //logger.Trace("Sample trace message");
+            //logger.Debug("Sample debug message");
+            //logger.Info("Sample informational message");
+            //logger.Warn("Sample warning message");
+            //logger.Error("Sample error message");
+            //logger.Fatal("Sample fatal error message");
+
+            // alternatively you can call the Log() method
+            // and pass log level as the parameter.
+            //logger.Log(LogLevel.Info, "Sample informational message");
+            logger.Error(new Exception("Error ceshi"), "Something bad happened");
+        }
+    }
+}
