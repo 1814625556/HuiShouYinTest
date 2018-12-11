e06545e4c42fa646820a2f70ab27d6f5090a13ce
chenchang <chenchang@kezaihui.com>
2018-12-11 18:44:10 +0800

    derby数据库调通

---
+++ Derby8e/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Derby8e
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            demo();
+
+            Console.ReadKey();
+        }
+
+        static void demo()
+        {
+            DataTable dt = new DataTable("Data");
+            var list = zaihui.dbs.derby.derbynet.GetData("SELECT ID,USERID,MEMBERID,PACKAGEID FROM ORDER_DETAIL", "F:\\derby\\db-derby-10.14.2.0-bin-success\\bin\\db");
+            var count = list.size();
+            for (var i = 0; i < count; i++)
+            {
+                var strList = list.get(i).ToString().Split(new char[]{','}, StringSplitOptions.RemoveEmptyEntries);
+                if (i == 0)
+                {
+                    foreach (var str in strList)
+                    {
+                        dt.Columns.Add(str, Type.GetType("System.String"));
+                    }
+                }
+                else
+                {         
+                    dt.Rows.Add(strList); 
+                }
+            }
+            Console.WriteLine($"rows:{dt.Rows.Count}");
+            Console.WriteLine("转化完毕~~~~");
+        }
+
+        static void strSplitTest()
+        {
+            string str = "ID,USERID,MEMBERID,PACKAGEID,PRODUCTID,CATEGORYID,PRODUCTMAKEID,ORDERID,PARENTID,POINT,ISBOOKING,PRODUCTTYPE,COUNT,ISCOOKED,ISCOOKING,COOKINGTIME,COOKINGFIRE,DELIVERCOUNT,CANCELCOUNT,REMINDCOUNT,PRINTCOUNT,ISSELFGIFT,ISORDERDISCOUNT,ISSELFDISCOUNT,ISSUPERPOSEDDISCOUNT,APPLYCANCELCOUNT,ISPACKAGE,ORDERMETHOD,SALETYPE,STATUS,COUPONTYPE,DISCOUNTTYPE,RECOMMENDTYPE,STAR,MAKETYPE,PACKAGETYPE,MODULEKEY,PRICETYPEKEY,PRODUCTNAME,PRODUCTCODE,UNIT,WEIGHTUNIT,ISPICKPRODUCT,ISHANDCHANGED,REMARK,CANCELREASONID,CANCELREASONTEXT,PROPERTYTEXT,METHODTEXT,COST,BASEPRICE,TIMEPRICE,PACKAGEPRICE,BASECOUPON,BASEDISCOUNT,METHODPRICE,PROPERTYPRICE,WEIGHT,TRUEPRICE,TRUEAMOUNT,ORDERDISCOUNTAMOUNT,REMINDLASTTIME,DEFAULTPRICE,GIFTREASONID,GIFTREASONTEXT,COOKINGTIMESECOND,RETURNTIME,REFUNDCOUNT,UPDATETIME,CREATETIME,ISUPLOAD,ISDELETE,";
+            //var str = list.get(0).ToString();
+            var arr = str.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+    }
+}
---
+++ DianCaijiDerbyTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DianCaijiDerbyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //var conn = new DerbyNET(@"F:\derby\db-derby-10.5.1.1-bin\bin\test1;create=true");
+                //var cc = conn.openConnection();
+                //var dt = conn.getRS("select * from ORDER_INFO");
+                //conn.closeConnection();
+                string str = zaihui.dbs.derby.derbynet.SayHello();
+                Console.WriteLine(str);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+            
+        }
+    }
+}
